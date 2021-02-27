a765075d05b3451ecbddf1d64edb32516863fcf4
Yashodha Athureliyage <yashodha.athureliyage@growonline.co.nz>
2021-02-28 04:04:17 +1300

    End of Section 6

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,25 +15,35 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function ActivityDashboard({activities, selectedActivity, 
-        selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity}: Props) {
+        selectActivity, cancelSelectActivity, editMode, 
+        openForm, closeForm, createOrEdit, 
+        deleteActivity, submitting}: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
                 <ActivityList activities={activities} 
                     selectActivity={selectActivity} 
-                    deleteActivity={deleteActivity}/>
+                    deleteActivity={deleteActivity}
+                    submitting={submitting}/>
             </Grid.Column>
             <Grid.Column width='6'>
                 {selectedActivity && !editMode &&
-                <ActivityDetails activity={selectedActivity} 
-                cancelSelectActivity={cancelSelectActivity}
-                openForm = {openForm}
-                />}
+                <ActivityDetails 
+                    activity={selectedActivity} 
+                    cancelSelectActivity={cancelSelectActivity}
+                    openForm = {openForm}
+                    />}
                 {editMode && 
-                <ActivityForm closeForm = {closeForm} activity = {selectedActivity} createOrEdit={createOrEdit} />}
+                <ActivityForm 
+                    submitting ={submitting} 
+                    closeForm = {closeForm} 
+                    activity = {selectedActivity} 
+                    createOrEdit={createOrEdit} 
+                />}
             </Grid.Column>
         </Grid>
     )
